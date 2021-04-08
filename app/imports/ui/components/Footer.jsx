fcd29eb45c5aa048910ce29f205b2128587a2bff
Leeden Raquel <77713467+leedenkraquel@users.noreply.github.com>
2021-04-07 14:37:34 -1000

    changed logo to be circular

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -8,7 +9,7 @@ class Footer extends React.Component {
       <footer>
         <div style={divStyle} className="ui center aligned container">
           <hr />
-          <img size='tiny' src='/images/gitclubs-logo.png' /> <br />
+          <Image size='tiny' circular src="/images/gitclubs-logo.png" /> <br />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />
