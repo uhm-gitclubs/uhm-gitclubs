75f50ae9b4eade396d2084805127ff877642a102
Mujtaba-Quadri <mujtabaq@hawaii.edu>
2021-04-12 16:26:36 -1000

    issue #3 complete

--- app/imports/ui/pages/ListStuff.jsx
+++ app/imports/ui/pages/ListStuff.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Loader, Grid, Icon, Card, Image, Button, Label } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
-import StuffItem from '../components/StuffItem';
+//import StuffItem from '../components/StuffItem';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListStuff extends React.Component {
@@ -17,22 +17,116 @@ class ListStuff extends React.Component {
   // Render the page once subscriptions have been received.
   renderPage() {
     return (
-      <Container>
-        <Header as="h2" textAlign="center">List Stuff</Header>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Condition</Table.HeaderCell>
-              <Table.HeaderCell>Edit</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.props.stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
-          </Table.Body>
-        </Table>
-      </Container>
+      <Grid container columns='equal'>
+        <Grid.Row>
+          <Container>
+            <Header as="h2" textAlign="center" >
+              <Icon color='green' name='home'/>My Clubs</Header>
+            <hr/></Container>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={5}>
+            <Card>
+              <Image src='https://acmanoa.github.io/assets/img/officers/placehold.png' wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>Association for Computing Machinery at UH Manoa (ACManoa/ACM)</Card.Header>
+                <Card.Meta>
+                  <span className='date'>Joined in 4/20/2019</span>
+                </Card.Meta>
+                <Card.Meta>
+                  <span className='expiration'>Expires on 6/9/2023</span>
+                </Card.Meta>
+                <Card.Description>
+                  <p>We, the Association for Computing Machinery at UH Manoa (ACManoa/ACM), are the largest computer science student organization at the University of Hawaii at Manoa.</p>
+                  <p>We welcome students of all different backgrounds, interests, and skill levels to join our community and share our passion for computer science.</p>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                    202 Members
+                </a>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='grey'>
+                    <Icon color='grey' name='info'/>
+                      More Info
+                  </Button>
+                  <Button basic color='red'>
+                    <Icon color='red' name='delete'/>
+                      Leave
+                  </Button>
+                </div>
+              </Card.Content>
+              <Card.Content>
+                <Label >
+                    Programming
+                  <Icon name='delete' />
+                </Label>
+                <Label >
+                    Hacking
+                  <Icon name='delete' />
+                </Label>
+                <Label >
+                    Computers
+                  <Icon name='delete' />
+                </Label>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column>
+            <Card>
+              <Image src='https://pbs.twimg.com/profile_images/1119496798874136576/3ao966FS.jpg' wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>Anime and Manga Society</Card.Header>
+                <Card.Meta>
+                  <span className='date'>Joined in 5/23/2019</span>
+                </Card.Meta>
+                <Card.Meta>
+                  <span className='expiration'>Expires on 8/5/2023</span>
+                </Card.Meta>
+                <Card.Description>
+                    We are an open community of people who love to watch Japanese anime, read manga, have an interest in gaming, cosplay, Japanese culture, western comics/animation, or anything related.
+                  <p>We meet at and around the UH Manoa campus for weekly anime showings and gaming meetups.</p>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                    67 Members
+                </a>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='grey'>
+                    <Icon color='grey' name='info'/>
+                      More Info
+                  </Button>
+                  <Button basic color='red'>
+                    <Icon color='red' name='delete'/>
+                      Leave
+                  </Button>
+                </div>
+              </Card.Content>
+              <Card.Content>
+                <Label >
+                    Gaming
+                  <Icon name='delete' />
+                </Label>
+                <Label >
+                    Anime
+                  <Icon name='delete' />
+                </Label>
+                <Label >
+                    Manga
+                  <Icon name='delete' />
+                </Label>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
