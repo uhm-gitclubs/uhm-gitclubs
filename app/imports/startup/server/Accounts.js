52344212b873aae6f0ee4bc35fa5fe12b63d0d7f
yongu2000 <yongu200@hawaii.edu>
2021-04-21 04:50:30 +0900

    Revert "Revert "Merge branch 'issue-26-publications' into issue-27-M2""
    This reverts commit 21193aac9d7708c015d9973e53f50614e3e6108a.

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,23 +4,19 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role, clubs, interests) {
+function createUser(email, password, role, interests) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
-    joined: clubs[0],
-    moderator: clubs[1],
-    interests: interests,
+    profile: {
+      interests: interests,
+    },
   });
   if (role === 'siteAdmin') {
     Roles.createRole(role, { unlessExists: true });
     Roles.addUsersToRoles(userID, 'siteAdmin');
-  }
-  if (role === 'clubModerator') {
-    Roles.createRole(role, { unlessExists: true });
-    Roles.addUsersToRoles(userID, 'clubModerator');
   }
 }
 
@@ -28,7 +24,7 @@ function createUser(email, password, role, clubs, interests) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role, clubs, interests }) => createUser(email, password, role, clubs, interests));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, interests }) => createUser(email, password, role, interests));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
