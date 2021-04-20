9b1295e3f6ab3e134e92e3d139c2524572914edd
yongu2000 <yongu200@hawaii.edu>
2021-04-21 04:29:09 +0900

    Update MyClubs

--- app/imports/ui/components/MyClub.jsx
+++ app/imports/ui/components/MyClub.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Card, Image, Label, Icon, Button } from 'semantic-ui-react';
+import { Card, Image, Label, Icon, Button, Modal, Confirm } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { _ } from 'meteor/underscore';
+import swal from 'sweetalert';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Club extends React.Component {
+  state = { open: false }
+
+  open = () => this.setState({ open: true })
+
+  handleConfirm = () => this.setState({ result: swal('Done!', '', 'success'), open: false })
+
+  handleCancel = () => this.setState({ open: false })
+
   render() {
     return (
       <Card>
@@ -26,14 +35,27 @@ class Club extends React.Component {
         </Card.Content>
         <Card.Content extra>
           <div className='ui two buttons'>
-            <Button basic color='grey'>
-              <Icon color='grey' name='info'/>
-              More Info
-            </Button>
-            <Button basic color='red'>
-              <Icon color='red' name='close'/>
+            <Modal
+              trigger={<Button basic color='grey' >
+                <Icon color='grey' name='info'/>
+                More Info
+              </Button>}
+              header='More Info'
+              content='Enter more specific details of club here.'
+              actions={[{ key: 'done', content: 'Done', positive: true }]}
+            />
+            <Button basic color='red' onClick={this.open}>
+              <Icon color='red' name='delete'/>
               Leave
             </Button>
+            <Confirm
+              open={this.state.open}
+              content='Are you sure you want to do this?'
+              cancelButton='Never mind'
+              confirmButton="Let's do it"
+              onCancel={this.handleCancel}
+              onConfirm={this.handleConfirm}
+            />
           </div>
         </Card.Content>
         <Card.Content extra>
