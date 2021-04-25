40d8e0b329207ec20cbaaa1dab71996f0ad0539a
yongu2000 <yongu200@hawaii.edu>
2021-04-25 12:12:12 +0900

    Need to sort and show only 3 club

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,10 +1,18 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Grid, Image, Header, Icon, Button, Container, Statistic, Card, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Clubs } from '../../api/club/Clubs';
+import Club from '../components/Club';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
   render() {
+    const totalSize = Clubs.collection.find().count();
+    const totalAccount = Meteor.users.find().count();
     const gridStyle = { height: '500px' };
     const gridStyle2 = { margin: '120px 0px 100px 0px' };
     const gridStyle3 = {
@@ -54,13 +62,13 @@ class Landing extends React.Component {
             <Statistic.Group widths='four'>
               <Statistic>
                 <Statistic.Value>
-                  <Icon name='user' />1300
+                  <Icon name='user' />{totalAccount}
                 </Statistic.Value>
                 <Statistic.Label>Users</Statistic.Label>
               </Statistic>
 
               <Statistic>
-                <Statistic.Value>250</Statistic.Value>
+                <Statistic.Value>{totalSize}</Statistic.Value>
                 <Statistic.Label>Clubs</Statistic.Label>
               </Statistic>
 
@@ -93,106 +101,9 @@ class Landing extends React.Component {
           </Grid>
           <Grid container columns='equal'>
             <Grid.Row>
-              <Grid.Column width={5}>
-                <Card>
-                  <Image src='https://acmanoa.github.io/assets/img/officers/placehold.png' wrapped ui={false} />
-                  <Card.Content>
-                    <Card.Header>Association for Computing Machinery at UH Manoa (ACManoa/ACM)</Card.Header>
-                    <Card.Meta>
-                      <span className='date'>Joined in 4/20/2019</span>
-                    </Card.Meta>
-                    <Card.Meta>
-                      <span className='expiration'>Expires on 6/9/2023</span>
-                    </Card.Meta>
-                    <Card.Description>
-                      <p>We, the Association for Computing Machinery at UH Manoa (ACManoa/ACM), are the largest computer science student organization at the University of Hawaii at Manoa.</p>
-                      <p>We welcome students of all different backgrounds, interests, and skill levels to join our community and share our passion for computer science.</p>
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <a>
-                      <Icon name='user' />
-                      202 Members
-                    </a>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <div className='ui two buttons'>
-                      <Button basic color='grey'>
-                        <Icon color='grey' name='info'/>
-                        More Info
-                      </Button>
-                      <Button basic color='red'>
-                        <Icon color='red' name='delete'/>
-                        Leave
-                      </Button>
-                    </div>
-                  </Card.Content>
-                  <Card.Content>
-                    <Label >
-                      Programming
-                      <Icon name='delete' />
-                    </Label>
-                    <Label >
-                      Hacking
-                      <Icon name='delete' />
-                    </Label>
-                    <Label >
-                      Computers
-                      <Icon name='delete' />
-                    </Label>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card>
-                  <Image src='https://pbs.twimg.com/profile_images/1119496798874136576/3ao966FS.jpg' wrapped ui={false} />
-                  <Card.Content>
-                    <Card.Header>Anime and Manga Society</Card.Header>
-                    <Card.Meta>
-                      <span className='date'>Joined in 5/23/2019</span>
-                    </Card.Meta>
-                    <Card.Meta>
-                      <span className='expiration'>Expires on 8/5/2023</span>
-                    </Card.Meta>
-                    <Card.Description>
-                      We are an open community of people who love to watch Japanese anime, read manga, have an interest in gaming, cosplay, Japanese culture, western comics/animation, or anything related.
-                      <p>We meet at and around the UH Manoa campus for weekly anime showings and gaming meetups.</p>
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <a>
-                      <Icon name='user' />
-                      67 Members
-                    </a>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <div className='ui two buttons'>
-                      <Button basic color='grey'>
-                        <Icon color='grey' name='info'/>
-                        More Info
-                      </Button>
-                      <Button basic color='red'>
-                        <Icon color='red' name='delete'/>
-                        Leave
-                      </Button>
-                    </div>
-                  </Card.Content>
-                  <Card.Content>
-                    <Label >
-                      Gaming
-                      <Icon name='delete' />
-                    </Label>
-                    <Label >
-                      Anime
-                      <Icon name='delete' />
-                    </Label>
-                    <Label >
-                      Manga
-                      <Icon name='delete' />
-                    </Label>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
+              <Card.Group centered>
+                {this.props.clubs.map((club, index) => <Club key={index} club={club}/>)}
+              </Card.Group>
             </Grid.Row>
           </Grid>
         </div>
@@ -201,4 +112,17 @@ class Landing extends React.Component {
   }
 }
 
-export default Landing;
+Landing.propTypes = {
+  clubs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+export default withTracker(() => {
+  const subscription = Meteor.subscribe(Clubs.nonuserPublicationName);
+  const ready = subscription.ready();
+  const clubs = Clubs.collection.find({}).fetch();
+  return {
+    clubs,
+    ready,
+  };
+})(Landing);
