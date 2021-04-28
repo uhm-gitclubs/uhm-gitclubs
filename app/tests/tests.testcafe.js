3cfcf96303c1bf983a071815e26c0464f77ea870
alvinedrada <aedrada@hawaii.edu>
2021-04-27 20:43:08 -1000

    implemented availability testing for signup

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -2,14 +2,15 @@ import { landingPage } from './landing.page';
 import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
 import { navBar } from './navbar.component';
+import { signupPage } from './signup.page';
 
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const credentials = { username: 'john@foo.com', password: 'changeme' };
+const credentials = { username: 'admin@foo.com', password: 'changeme', firstName: 'Admin', lastName: 'Poop' };
 
 fixture('meteor-application-template-react localhost test with default db')
-    .page('http://localhost:3000');
+  .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
   await landingPage.isDisplayed(testController);
@@ -19,6 +20,15 @@ test('Test that signin and signout work', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that signup works, then logout works', async (testController) => {
+  const newUser = `user-${new Date().getTime()}@foo.com`;
+  await navBar.gotoSignupPage(testController);
+  await signupPage.isDisplayed(testController);
+  await signupPage.signupUser(testController, newUser, credentials.password, credentials.firstName, credentials.lastName);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
 });
